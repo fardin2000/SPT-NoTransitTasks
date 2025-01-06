c2f3a49f64b54aa5fd20880c70f8789281d8f0e5
Bob S <shibdib@users.noreply.github.com>
2025-01-06 02:35:01 -0500

    1.0.1 code cleanup

--- src/mod.js
+++ src/mod.js
@@ -2,58 +2,47 @@
 
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require('fs');
+let count = 0;
 
-class NoTransitTasks 
-{   
-    postDBLoad(container) {
+class NoTransitTasks
+{
+	postDBLoad(container) {
 		const logger = container.resolve("WinstonLogger");
-        const DB = container.resolve("DatabaseService").getTables();
+		const DB = container.resolve("DatabaseService").getTables();
 		const quests = Object.values(DB.templates.quests);
-		
+
+		const fixTask = (questId, transitId, oneSession = false) => {
+			const quest = quests.find((q) => q._id === questId);
+			quest.conditions.AvailableForFinish = quest.conditions.AvailableForFinish.filter((c) => c.id !== transitId);
+			if (oneSession) quest.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false);
+			count++;
+		};
+
 		// Special Comms
-		// Remove the transit, rest is fine
-		const specialComms = quests.find((q) => q._id === "66ab970848ddbe9d4a0c49a8");
-		specialComms.conditions.AvailableForFinish = specialComms.conditions.AvailableForFinish.filter((c) => c.id !== "66ab970848ddbe9d4a0c49ab");
-		
-		// Foresters Duty
-		// Remove the transit, remove the session requirement
-		const forester = quests.find((q) => q._id === "66ab9da7eb102b9bcd08591c");
-		forester.conditions.AvailableForFinish = forester.conditions.AvailableForFinish.filter((c) => c.id !== "66ab9da7eb102b9bcd08591f");
-		forester.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false)
-		
-		// New Day New Paths
-		// Remove the transit, remove the session requirement
-		const newDay = quests.find((q) => q._id === "66aa58245ab22944110db6e9");
-		newDay.conditions.AvailableForFinish = newDay.conditions.AvailableForFinish.filter((c) => c.id !== "66aa5be8035c6a410dc570b2");
-		newDay.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false)
-		
-		// Know your place
-		// Remove the transit, remove the session requirement
-		const knowYourPlace = quests.find((q) => q._id === "66aa61663aa37705c5024277");
-		knowYourPlace.conditions.AvailableForFinish = knowYourPlace.conditions.AvailableForFinish.filter((c) => c.id !== "66aa61663aa37705c502427c");
-		knowYourPlace.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false)
-		
-		// Secrets of Polikhim
-		// Remove the transit, remove the session requirement
-		const secrets = quests.find((q) => q._id === "66aa74571e5e199ecd094f18");
-		secrets.conditions.AvailableForFinish = secrets.conditions.AvailableForFinish.filter((c) => c.id !== "66aa74571e5e199ecd094f1b");
-		secrets.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false)
-		
-		// Beneath the streets
-		// Remove the transit, remove the session requirement
-		const streets = quests.find((q) => q._id === "66aba85403e0ee3101042877");
-		streets.conditions.AvailableForFinish = streets.conditions.AvailableForFinish.filter((c) => c.id !== "66aba85403e0ee310104287a");
-		streets.conditions.AvailableForFinish.forEach((c) => c.oneSessionOnly = false)
-		
+		fixTask("66ab970848ddbe9d4a0c49a8", "66ab970848ddbe9d4a0c49ab");
+
 		// Cream of the Crop
-		// Remove the transit, rest is fine
-		const cream = quests.find((q) => q._id === "66e01ae0c391e4c94903d220");
-		cream.conditions.AvailableForFinish = cream.conditions.AvailableForFinish.filter((c) => c.id !== "66e2c832596e2895181e1bd4");
-		
+		fixTask("66e01ae0c391e4c94903d220", "66e2c832596e2895181e1bd4");
+
 		// Sample IV
-		// Remove the transit, rest is fine
-		const sampleIv = quests.find((q) => q._id === "67040c78bf4be8a4ef041a65");
-		sampleIv.conditions.AvailableForFinish = sampleIv.conditions.AvailableForFinish.filter((c) => c.id !== "67262b4023c03479138728fa");
+		fixTask("67040c78bf4be8a4ef041a65", "67262b4023c03479138728fa");
+
+		// Foresters Duty
+		fixTask("66ab9da7eb102b9bcd08591c", "66ab9da7eb102b9bcd08591f", true);
+
+		// New Day New Paths
+		fixTask("66aa58245ab22944110db6e9", "66aa5be8035c6a410dc570b2", true);
+
+		// Know your place
+		fixTask("66aa61663aa37705c5024277", "66aa61663aa37705c502427c", true);
+
+		// Secrets of Polikhim
+		fixTask("66aa74571e5e199ecd094f18", "66aa74571e5e199ecd094f1b", true);
+
+		// Beneath the streets
+		fixTask("66aba85403e0ee3101042877", "66aba85403e0ee310104287a", true);
+
+		logger.info(`[NoTransitTasks]: Altered ${count} transit tasks.`);
 	}
 }
 
